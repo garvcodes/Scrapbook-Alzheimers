08474f353c8f4a4d3945b10cd210a7164ca64d8a
ronitjain10 <ronitrjain@berkeley.edu>
2023-10-28 20:21:22 -0700

    mindsDB connected

--- app/mindsdb/mindsdb.js
+++ app/mindsdb/mindsdb.js
@@ -1,14 +1,55 @@
-import MindsDB from 'mindsdb-js-sdk';
+const MindsDB = require("mindsdb-js-sdk").default;
 
-// NOTE: If you're using CommonJS module syntax instead of ES6 imports:
-// const MindsDB = require("mindsdb-js-sdk").default;
-
-try {
-  await MindsDB.connect({
-    user: 'garv5114@gmail.com',
-    password: 'Garvsoham1115!'
-  });
-} catch(error) {
-  // Failed to authenticate.
+let mindsDBConnect = async function() { 
+    try {
+        await MindsDB.connect({
+            user: 'ronitrjain@gmail.com',
+            password: 'MaggieWalker1!'
+        });
+    console.log('connected');
+    return true;
+    
+    } catch(error) {
+    // Failed to authenticate
+    console.log(error);
+    }
 }
 
+const connectionParams = {
+    'host': 'mongodb+srv://garv5114:62712810@nailfungus.ocp1zst.mongodb.net/',
+    'database': 'store_prompts' 
+  }
+  
+let connecttoMongo = async function() { 
+  try {
+    const mongoDatabase = await MindsDB.Databases.createDatabase(
+      'scrapbook_js_db', // Name of the datasource
+      'mongodb',          // Type of the database
+      connectionParams, // Connection parameters
+    );
+    console.log('Connected to MongoDB successfully!');
+  } catch (error) {
+    throw(error);
+  }
+}
+
+let main = (async function() {
+    const isConnected = await mindsDBConnect();
+    console.log(isConnected);
+    if (isConnected) {
+        await connecttoMongo();
+        }
+    
+});
+
+let fetchData = async function() {
+    try {
+        const data = await MindsDB.Databases.getDatabase('scrapbook_js_db').query('SELECT * FROM store_prompts');
+        console.log(data);
+    } catch (error) {
+        throw(error);
+    }
+}
+console.log(fetchData());
+
+  
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -19,28 +19,34 @@ const PromptCardList = ({ data, handleTagClick }) => {
   );
 };
 
+// Import necessary hooks and functions
 const Feed = () => {
+  // Retrieve session data using a custom hook
   const { data: session } = useSession();
+
+  // State to store all posts
   const [allPosts, setAllPosts] = useState([]);
 
-  // Search states
-  const [searchText, setSearchText] = useState("");
-  const [searchTimeout, setSearchTimeout] = useState(null);
-  const [searchedResults, setSearchedResults] = useState([]);
+  // States related to the search functionality
+  const [searchText, setSearchText] = useState("");           // Text entered in the search input
+  const [searchTimeout, setSearchTimeout] = useState(null);   // Timeout for debouncing the search
+  const [searchedResults, setSearchedResults] = useState([]); // Results after searching
 
+  // Fetch all posts from the API
   const fetchPosts = async () => {
     const response = await fetch("/api/prompt");
     const data = await response.json();
-
     setAllPosts(data);
   };
-
+  console.log("Search", searchText) 
+  // Fetch posts when the component mounts
   useEffect(() => {
     fetchPosts();
   }, []);
 
+  // Function to filter posts based on the search text
   const filterPrompts = (searchText) => {
-    const regex = new RegExp(searchText, "i"); // 'i' flag for case-insensitive search
+    const regex = new RegExp(searchText, "i"); // Case-insensitive search
     return allPosts.filter(
       (item) =>
         regex.test(item.creator.username) ||
@@ -49,11 +55,15 @@ const Feed = () => {
     );
   };
 
+  // Handle changes in the search input
   const handleSearchChange = (e) => {
-    clearTimeout(searchTimeout);
-    setSearchText(e.target.value);
+    
+    clearTimeout(searchTimeout); // Clear any existing timeout
+    setSearchText(e.target.value); // Update the search text state
 
-    // Debounce method
+    
+
+    // Debounce the search to avoid excessive computations
     setSearchTimeout(
       setTimeout(() => {
         const searchResult = filterPrompts(e.target.value);
@@ -61,15 +71,18 @@ const Feed = () => {
       }, 500)
     );
   };
+  console.log("Search", searchText) 
 
+  // Handle when a tag is clicked
   const handleTagClick = (tagName) => {
-    setSearchText(tagName);
-
+    setSearchText(tagName); // Set the search text to the tag name
     const searchResult = filterPrompts(tagName);
     setSearchedResults(searchResult);
   };
 
+  // Render the component
   return (
+    // Only render if there's a user session
     session?.user && (
       <section className='feed'>
         <form className='relative w-full flex-center'>
@@ -82,8 +95,7 @@ const Feed = () => {
             className='search_input peer'
           />
         </form>
-
-        {/* All Prompts */}
+        {/* Display the search results if there's any search text, otherwise display all posts */}
         {searchText ? (
           <PromptCardList
             data={searchedResults}
@@ -97,4 +109,5 @@ const Feed = () => {
   );
 };
 
+// Export the Feed component
 export default Feed;
